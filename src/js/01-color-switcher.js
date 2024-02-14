041fca63fe80a8d9cb07f56ff30728c2b3404196
Canisa21 <jablonska.paul@gmail.com>
2024-02-14 22:08:53 +0100

    task03_solved

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const body = document.body;
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
+const bodyHtml = document.body;
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 let timerId = null;
 
-startBtn.addEventListener('click', event => {
+startButton.addEventListener('click', event => {
   timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
+    bodyHtml.style.backgroundColor = getRandomHexColor();
   }, 1000);
   event.currentTarget.setAttribute('disabled', 'disabled');
 });
 
-stopBtn.addEventListener('click', () => {
+stopButton.addEventListener('click', () => {
   clearInterval(timerId);
-  startBtn.removeAttribute('disabled');
+  startButton.removeAttribute('disabled');
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix'
+
+const form = document.querySelector('.form');
+
+const handleForm = event => {
+  event.preventDefault();
+  const firstDelay = parseInt(form.elements.delay.value, 10);
+  const delayStep = parseInt(form.elements.step.value, 10);
+  const amount = parseInt(form.elements.amount.value, 10);
+  setTimeout(() => {
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, delayStep)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  }, firstDelay);
+};
+
+form.addEventListener('submit', handleForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay }); // Fulfill
+    } else {
+      reject({ position, delay }); // Reject
+    }
+  });
 }
